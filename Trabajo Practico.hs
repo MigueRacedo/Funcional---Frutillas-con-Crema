550c83b4ec2b513fb16d6e5f5b2c60e71a38e07d
Phiiliip <felilotero@gmail.com>
2018-04-18 19:16:24 -0300

    Creo el tipo de MC

--- Trabajo Practico.hs
+++ Trabajo Practico.hs
@@ -1,1 +1,7 @@
+data MicroControlador = MicroControlador [Posicion] AcumuladorA AcumuladorB ProgramCounter Etiqueta
 
+type Posicion = Int
+type AcumuladorA = Int
+type AcumuladorB = Int
+type ProgramCounter = Int
+type Etiqueta = String
