0ed79661e51abf23a9fbb07bedc9241366fe5f0e
Phiiliip <felilotero@gmail.com>
2018-04-18 19:23:05 -0300

    Creo los getters

--- Trabajo Practico.hs
+++ Trabajo Practico.hs
@@ -5,3 +5,18 @@ type AcumuladorA = Int
 type AcumuladorB = Int
 type ProgramCounter = Int
 type Etiqueta = String
+
+posiciones :: MicroControlador -> [Int]
+posiciones (MicroControlador lista _ _ _ _) = lista
+
+acumuladorA :: MicroControlador -> Int
+acumuladorA (MicroControlador _ a _ _ _) = a
+
+acumuladorB :: MicroControlador -> Int
+acumuladorB (MicroControlador _ _ b _ _) = b
+
+programCounter :: MicroControlador -> Int
+programCounter (MicroControlador _ _ _ pc _) = pc
+
+etiqueta :: MicroControlador -> String
+etiqueta (MicroControlador _ _ _ _ et) = et
