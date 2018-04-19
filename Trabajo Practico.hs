cea8ca5e969e86c2ecd71de11ac6221722e55643
Phiiliip <felilotero@gmail.com>
2018-04-19 00:57:53 -0300

    Funcioens de la tabla

--- Trabajo Practico.hs
+++ Trabajo Practico.hs
@@ -1,6 +1,6 @@
 -- Definicion de tipos y de Data:
 
-data MicroControlador = UnMicroControlador {posiciones::[Posicion],acumuladorA::Int,acumuladorB::Int,programCounter::Int,etiqueta::String} deriving (Show)
+data MicroControlador = UnMicroControlador {posiciones::[Posicion],acumuladorA::Int,acumuladorB::Int,programCounter::Int,etiqueta::String} deriving (Eq,Show)
 
 type Posicion = Int
 
@@ -14,7 +14,19 @@ tuMami = UnMicroControlador {
 }
 
 nOP :: MicroControlador -> MicroControlador
-nOP (UnMicroControlador _ _ _ programCounter _) = UnMicroControlador { programCounter = incrementarPC microControlador}
+nOP microControlador = UnMicroControlador { programCounter = incrementarPC microControlador}
+
+aDD :: MicroControlador -> MicroControlador
+aDD microControlador = UnMicroControlador { acumuladorA = (acumuladorA microControlador + acumuladorB microControlador), acumuladorB = 0}
+
+dIV :: MicroControlador -> MicroControlador
+dIV microControlador = UnMicroControlador { acumuladorA = (acumuladorA microControlador `div` acumuladorB microControlador), acumuladorB = 0}
+
+swap :: MicroControlador -> MicroControlador
+swap microControlador = UnMicroControlador { acumuladorA = (acumuladorB microControlador), acumuladorB = (acumuladorA microControlador)}
+
+lod :: MicroControlador -> Int -> MicroControlador
+lod microControlador addr = UnMicroControlador {}
 
 -- Funciones Auxiliares:
 
