5a2c4bd865fc460b12474ab9c2f62e05b504b6cf
Phiiliip <felilotero@gmail.com>
2018-04-25 12:56:19 -0300

    Agrego todos los casos de testing
    Entrega 1 fianlizada

--- Trabajo Practico.hs
+++ Trabajo Practico.hs
@@ -9,7 +9,7 @@ type Instruccion = MicroControlador -> MicroControlador
 type MicroInstruccion = MicroControlador -> MicroControlador
 
 xt8088 = UnMicroControlador {
-    posiciones = [],
+    posiciones = (replicate 1024 0),
     acumuladorA = 0,
     acumuladorB = 0,
     programCounter = 0,
@@ -29,8 +29,8 @@ divide microControlador = (dividirAcumuladores.incrementarPC) microControlador
 swap :: Instruccion
 swap microControlador = (intercambiarAcumuladores.incrementarPC) microControlador
 
-lod :: MicroControlador -> Int -> MicroControlador
-lod microControlador addr = ((setearAcumuladorA (sacarDeLista addr (posiciones microControlador))).incrementarPC) microControlador
+lod :: Int -> Instruccion
+lod addr microControlador = ((setearAcumuladorA (sacarDeLista addr (posiciones microControlador))).incrementarPC) microControlador
 
 str :: Int -> Int -> Instruccion
 str addr val microControlador = ((partirListaYColocarEn addr val).incrementarPC) microControlador
@@ -49,6 +49,9 @@ dividirAcumuladores microControlador  = microControlador { acumuladorA = (acumul
 setearAcumuladorA :: Int -> MicroInstruccion
 setearAcumuladorA val microControlador = microControlador {acumuladorA = val}
 
+setearAcumuladorB :: Int -> MicroInstruccion
+setearAcumuladorB val microControlador = microControlador { acumuladorB = val}
+
 intercambiarAcumuladores :: MicroInstruccion
 intercambiarAcumuladores microControlador = microControlador { acumuladorA = (acumuladorB microControlador), acumuladorB = (acumuladorA microControlador)}
 
@@ -59,12 +62,36 @@ partirListaYColocarEn :: Int -> Int -> MicroInstruccion
 partirListaYColocarEn pos valor microControlador = microControlador {posiciones = partirListaYAgregar pos valor (posiciones microControlador)} 
 
 partirListaYAgregar :: Int -> Int -> [Int] -> [Int]
-partirListaYAgregar pos valor listaPos = (take pos listaPos) ++ [valor] ++ (drop pos listaPos)
+partirListaYAgregar pos valor listaPos = (take (pos-1) listaPos) ++ [valor] ++ (drop (pos-1) listaPos)
 
 sacarDeLista :: Int -> [Int] -> Int
 sacarDeLista pos listaPos = (!!) listaPos pos
 
--- Testing:
+-- Testing / Casos de prueba:
 
---Punto 3.3 : (add.(lodv 22).swap.(lodv 10)) xt8088
--- Punto 3.4:
+-- Punto 2: (nop.nop.nop) xt8088 ----> Correcto
+
+-- Punto 3 : lodv 5 xt8088 -----> Correcto
+-- Punto 3.1 : swap fp20 -----> Correcto
+-- Punto 3.2 : Correcto
+
+-- Punto 4 : str 2 5 at8086 -----> Correcto
+-- Punto 4.1 : lod 2 xt8088 -----> Correcto
+-- Punto 4.2 : (divide.(setearAcumuladorA 2).(setearAcumuladorB 0)) xt8088 ----> Correcto
+-- Punto 4.3 : (divide.(setearAcumuladorA 12).(setearAcumuladorB 4)) xt8088 -----> Correcto
+
+fp20 = UnMicroControlador {
+    posiciones = (replicate 1024 0),
+    acumuladorA = 7,
+    acumuladorB = 24,
+    programCounter = 0,
+    etiqueta = ""
+}
+
+at8086 = UnMicroControlador {
+    posiciones = [1..20],
+    acumuladorA = 0,
+    acumuladorB = 0,
+    programCounter = 0,
+    etiqueta = ""
+} 
