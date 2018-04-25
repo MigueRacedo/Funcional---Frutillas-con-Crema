ef2ce5233091791a21550faf2bd7af7f2f05ae01
Phiiliip <felilotero@gmail.com>
2018-04-25 12:34:31 -0300

    Funciones STR y LOD

--- Trabajo Practico.hs
+++ Trabajo Practico.hs
@@ -6,6 +6,8 @@ type Posicion = Int
 
 type Instruccion = MicroControlador -> MicroControlador
 
+type MicroInstruccion = MicroControlador -> MicroControlador
+
 xt8088 = UnMicroControlador {
     posiciones = [],
     acumuladorA = 0,
@@ -27,32 +29,42 @@ divide microControlador = (dividirAcumuladores.incrementarPC) microControlador
 swap :: Instruccion
 swap microControlador = (intercambiarAcumuladores.incrementarPC) microControlador
 
---lod :: MicroControlador -> Int -> MicroControlador
---lod microControlador addr = microControlador {}
+lod :: MicroControlador -> Int -> MicroControlador
+lod microControlador addr = ((setearAcumuladorA (sacarDeLista addr (posiciones microControlador))).incrementarPC) microControlador
 
---str :: Microcontrolador -> Int -> Int -> MicroControlador
---str microControlador addr val = microControlador {}
+str :: Int -> Int -> Instruccion
+str addr val microControlador = ((partirListaYColocarEn addr val).incrementarPC) microControlador
 
 lodv :: Int -> Instruccion
 lodv val microControlador = ((setearAcumuladorA val).incrementarPC) microControlador
 
 -- Funciones Auxiliares:
 
-incrementarPC :: MicroControlador -> MicroControlador
+incrementarPC :: MicroInstruccion
 incrementarPC microControlador = microControlador { programCounter = (programCounter microControlador) +1 }
 
-dividirAcumuladores :: MicroControlador -> MicroControlador
+dividirAcumuladores :: MicroInstruccion
 dividirAcumuladores microControlador  = microControlador { acumuladorA = (acumuladorA microControlador `div` acumuladorB microControlador), acumuladorB = 0}
 
-setearAcumuladorA :: Int -> MicroControlador -> MicroControlador
+setearAcumuladorA :: Int -> MicroInstruccion
 setearAcumuladorA val microControlador = microControlador {acumuladorA = val}
 
-intercambiarAcumuladores :: Instruccion
+intercambiarAcumuladores :: MicroInstruccion
 intercambiarAcumuladores microControlador = microControlador { acumuladorA = (acumuladorB microControlador), acumuladorB = (acumuladorA microControlador)}
 
-sumaryPonerEnA :: Instruccion
+sumaryPonerEnA :: MicroInstruccion
 sumaryPonerEnA microControlador = microControlador { acumuladorA = (acumuladorA microControlador + acumuladorB microControlador), acumuladorB = 0}
+
+partirListaYColocarEn :: Int -> Int -> MicroInstruccion
+partirListaYColocarEn pos valor microControlador = microControlador {posiciones = partirListaYAgregar pos valor (posiciones microControlador)} 
+
+partirListaYAgregar :: Int -> Int -> [Int] -> [Int]
+partirListaYAgregar pos valor listaPos = (take pos listaPos) ++ [valor] ++ (drop pos listaPos)
+
+sacarDeLista :: Int -> [Int] -> Int
+sacarDeLista pos listaPos = (!!) listaPos pos
 
 -- Testing:
 
 --Punto 3.3 : (add.(lodv 22).swap.(lodv 10)) xt8088
+-- Punto 3.4:
